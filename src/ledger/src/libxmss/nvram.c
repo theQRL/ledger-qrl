bcfa7d73e40a8f36ae2807d2915389c7a583b6b4
Juan Leni <lenijuan@gmail.com>
2018-11-15 12:32:49 +0100

    Splitting app specific code

--- src/ledger/src/libxmss/nvram.c
+++
@@ -1,5 +0,0 @@
-// Distributed under the MIT software license, see the accompanying
-// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
-#include "nvram.h"
-
-NVCONST N_DATA_t N_DATA_impl;
